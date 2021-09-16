48d1f20027c6015ef3f47cc8244a009bc5f26f29
Jallen <hunnidtekashi@gmail.com>
2021-09-16 19:40:46 +0200

    Final Commit

--- server.js
+++ server.js
@@ -4,8 +4,34 @@ const app = express();
 
 app.set("view engine", 'pug')
 
+app.use(express.static('public'))
+
 app.get('/', (req,res) => {
     res.render('index')
 })
 
-app.listen(7000, () => console.log('Server listening 7000'))
+const server = app.listen(7000, () => console.log('Server listening 7000'))
+
+//include socket io
+
+const io = require('socket.io')(server);
+
+io.on("connection", socket => {
+    console.log('new client connected!!');
+    
+    socket.username = "Jallen Hunnid";
+
+    socket.on("new_message", data => {
+        io.sockets.emit("new_message", {
+            message : data.message,
+            username: socket.username
+        });
+        socket.on('change_username', data => {
+            socket.username = data.username;
+        })
+        socket.on('typing', (data) => {
+            socket.broadcast.emit('typing', {username: socket.username}); 
+        })
+    });
+});
+
